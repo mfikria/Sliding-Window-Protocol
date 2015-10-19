f1911fde394461a8f32d9b9f944ed0285ec58d32
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2015-10-20 01:44:45 +0700

    Add Ack file

--- SWP_ack.h
+++ SWP_ack.h
@@ -1,6 +1,32 @@
+#ifndef _SWP_ACK_H_
+#define _SWP_ACK_H_
+
+#include "SWP_frame.h"
+
 typedef struct
 {
 	unsigned int ack;
 	Byte frameno;
 	unsigned short checksum;
 } ACK;
+
+unsigned int compareChecksum(FRAME frame) {
+	return calc_crc16(dataFrameToString(frame)) == frame.checksum ? ACK : NAK;
+}
+
+void createAck(ACK *ack, FRAME frame){
+	ack->ack = compareChecksum(frame);
+	ack->frameno = frame.frameno;
+	ack->checksum = calc_crc16(dataFrameToString(frame));
+}
+
+char* ackToString(ACK ack){
+    char* strack = (char*) malloc(ACKSIZE * sizeof(char));
+    strack[0] = ack.ack;
+    strack[1] = ack.frameno;
+    strack[2] = ack.checksum;
+		sprintf(strack, "%s%x", strack, frame.checksum);
+    return strack;
+}
+
+#endif
